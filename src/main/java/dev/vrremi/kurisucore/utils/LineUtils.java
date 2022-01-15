84aecf679e965ce6c9bcc54bf54e08edf2e12011
VRRemi <business@vrremi.xyz>
2022-01-15 09:55:10 +0800

    Update LineUtils.java

--- src/main/java/dev/vrremi/kurisucore/utils/LineUtils.java
+++ src/main/java/dev/vrremi/kurisucore/utils/LineUtils.java
@@ -12,5 +12,11 @@ public class LineUtils {
         addHeader(lines, sender, "&7");
     }
 
-    
+    public static void addHeader(List<String> lines, CommandSender sender, String color) {
+        if (sender instanceof Player) {
+            lines.add(CC.color(color + "&m" + StringUtils.repeat(" ", 50) + "&r"));
+        } else {
+            lines.add(CC.color(color + StringUtils.repeat("=", 50)));
+        }
+    }
 }
