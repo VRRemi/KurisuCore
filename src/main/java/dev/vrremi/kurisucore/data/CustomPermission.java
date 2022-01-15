92ec9cc95ec01ea49f9f45a01a2fc99cd0433d8e
VRRemi <business@vrremi.xyz>
2022-01-15 14:03:06 -0800

    Update CustomPermission.java

--- src/main/java/dev/vrremi/kurisucore/data/CustomPermission.java
+++ src/main/java/dev/vrremi/kurisucore/data/CustomPermission.java
@@ -1,4 +1,11 @@
 package dev.vrremi.kurisucore.data;
 
+import dev.vrremi.kurisucore.KurisuCore;
+import org.bukkit.entity.Player;
+import org.bukkit.permissions.PermissionBase;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
 public class CustomPermission {
 }
