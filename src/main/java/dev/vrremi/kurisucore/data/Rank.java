0c8ac8aee6cc2f1cc37ce1dba5c1cb9f2753766d
VRRemi <business@vrremi.xyz>
2022-01-15 13:39:24 -0800

    Create Rank.java

---
+++ src/main/java/dev/vrremi/kurisucore/data/Rank.java
@@ -0,0 +1,13 @@
+package dev.vrremi.kurisucore.data;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Getter
+public class Rank {
+
+    
+}
