38a09d4053b4385c93117e04e921f1efc6ed1b3f
VRRemi <business@vrremi.xyz>
2022-01-15 10:02:47 +0800

    Create Server.java

---
+++ src/main/java/dev/vrremi/kurisucore/utils/Server.java
@@ -0,0 +1,12 @@
+package dev.vrremi.kurisucore.utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class Server {
+
+    public static Player getOnline(String name) {
+        return Bukkit.getOnlinePlayers().stream().filter(online -> online.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
+    }
+    
+}
