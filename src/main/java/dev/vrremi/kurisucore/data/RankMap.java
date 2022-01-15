c54e09fc94ff021e3c74de51cdc518d810848f19
VRRemi <business@vrremi.xyz>
2022-01-15 13:42:43 -0800

    Create RankMap.java

---
+++ src/main/java/dev/vrremi/kurisucore/data/RankMap.java
@@ -0,0 +1,13 @@
+package dev.vrremi.kurisucore.data;
+
+import dev.vrremi.kurisucore.KurisuCore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class RankMap {
+
+}
