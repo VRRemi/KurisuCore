e8170bbe9f48dc0af9fd134b3e30224238508724
VRRemi <business@vrremi.xyz>
2022-01-15 13:45:57 -0800

    Update RankMap.java

--- src/main/java/dev/vrremi/kurisucore/data/RankMap.java
+++ src/main/java/dev/vrremi/kurisucore/data/RankMap.java
@@ -10,4 +10,10 @@ import java.util.stream.Collectors;
 
 public class RankMap {
 
+    private final Map<Rank, Long> rankMap = new HashMap<>();
+
+    public void addRank(Rank rank, long time) {
+        rankMap.put(rank, time);
+    }
+
 }
