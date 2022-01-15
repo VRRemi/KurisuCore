a6f7e2aae24371779b3c6be55cefabf34348fef1
VRRemi <business@vrremi.xyz>
2022-01-14 16:51:35 -0800

    Create CC.java

---
+++ src/main/java/dev/vrremi/kurisucore/utils/CC.java
@@ -0,0 +1,12 @@
+package dev.vrremi.kurisucore.utils;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+
+import java.awt.*;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
