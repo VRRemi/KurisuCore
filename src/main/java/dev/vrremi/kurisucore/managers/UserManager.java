2b573c33737e5b5d48b87ab65000780b8f592256
VRRemi <business@vrremi.xyz>
2022-01-15 12:03:15 -0800

    Create UserManager.java

---
+++ src/main/java/dev/vrremi/kurisucore/managers/UserManager.java
@@ -0,0 +1,15 @@
+package dev.vrremi.kurisucore.managers;
+
+import dev.vrremi.kurisucore.KurisuCore;
+import dev.vrremi.kurisucore.data.User;
+import dev.vrremi.kurisucore.utils.Threading;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.UUID;
+
+public class UserManager {
+}
