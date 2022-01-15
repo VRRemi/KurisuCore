c93b93c3c8dda1d8efc8109dee27edf0cf78d2a3
VRRemi <business@vrremi.xyz>
2022-01-15 10:08:05 +0800

    Create PlayerEvent.java

---
+++ src/main/java/dev/vrremi/kurisucore/events/PlayerEvent.java
@@ -0,0 +1,16 @@
+package dev.vrremi.kurisucore.events;
+
+import dev.vrremi.kurisucore.KurisuCore;
+import dev.vrremi.kurisucore.data.Punishment;
+import dev.vrremi.kurisucore.data.Rank;
+import dev.vrremi.kurisucore.data.Tag;
+import dev.vrremi.kurisucore.data.User;
+import dev.vrremi.kurisucore.data.menu.Menu;
+import dev.vrremi.kurisucore.managers.ConnectionPoolManager;
+import dev.vrremi.kurisucore.utils.CC;
+import dev.vrremi.kurisucore.utils.ConfigUtils;
+import dev.vrremi.kurisucore.utils.Threading;
+
+public class PlayerEvent {
+    
+}
