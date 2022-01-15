286656d0e126d9ab9ae201a41b44c26af4081c0c
VRRemi <business@vrremi.xyz>
2022-01-15 13:56:32 -0800

    Update Punishment.java

--- src/main/java/dev/vrremi/kurisucore/data/Punishment.java
+++ src/main/java/dev/vrremi/kurisucore/data/Punishment.java
@@ -6,4 +6,11 @@ import java.util.UUID;
 
 @Getter
 public class Punishment {
+
+    private final String id;
+    private final PunishmentType punishmentType;
+    private final String reason, punisher;
+    private final long created, timeout;
+
+    
 }
