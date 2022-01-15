2b07d1a5f7f38dca2ddec47c6a0920cb74cc827f
VRRemi <business@vrremi.xyz>
2022-01-15 13:49:32 -0800

    Update RankMap.java

--- src/main/java/dev/vrremi/kurisucore/data/RankMap.java
+++ src/main/java/dev/vrremi/kurisucore/data/RankMap.java
@@ -53,6 +53,21 @@ public class RankMap {
         return highestRank;
     }
 
+    public static RankMap convertFromString(String string) {
+        RankMap rankMap = new RankMap();
+        if (string.isEmpty()) return getDefault();
+        for (String rankPart : string.split(",")) {
+            if (rankPart.isEmpty()) continue;
+            String name = rankPart.split(":")[0];
+            long time = Long.parseLong(rankPart.split(":")[1]);
+            Rank rank = KurisuCore.getRankManager().getRank(name);
+            if (rank != null) {
+                rankMap.addRank(rank, time == 0 ? Long.MAX_VALUE : time);
+            }
+        }
+        return rankMap;
+    }
+
     
 
 }
