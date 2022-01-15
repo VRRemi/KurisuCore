67b3f4c39e40bdc30976ad30573901b8a72cd04e
VRRemi <business@vrremi.xyz>
2022-01-15 10:03:57 +0800

    Update Server.java

--- src/main/java/dev/vrremi/kurisucore/utils/Server.java
+++ src/main/java/dev/vrremi/kurisucore/utils/Server.java
@@ -8,5 +8,5 @@ public class Server {
     public static Player getOnline(String name) {
         return Bukkit.getOnlinePlayers().stream().filter(online -> online.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
     }
-    
+
 }
