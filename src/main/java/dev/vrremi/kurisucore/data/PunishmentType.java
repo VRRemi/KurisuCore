ed40c5cdabf5380630d2961e334b25d941936746
VRRemi <business@vrremi.xyz>
2022-01-15 13:51:47 -0800

    Create PunishmentType.java

---
+++ src/main/java/dev/vrremi/kurisucore/data/PunishmentType.java
@@ -0,0 +1,17 @@
+package dev.vrremi.kurisucore.data;
+
+import org.apache.commons.lang.WordUtils;
+
+public enum PunishmentType {
+
+    BAN,
+    TEMP_BAN,
+    UNBAN,
+    MUTE,
+    TEMP_MUTE,
+    UNMUTE,
+    KICK,
+    WARN;
+
+    
+}
