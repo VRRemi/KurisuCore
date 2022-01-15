b42a5264051d6bd11b12b1c9a5bbe19731faeb5f
VRRemi <business@vrremi.xyz>
2022-01-15 13:56:50 -0800

    Update Punishment.java

--- src/main/java/dev/vrremi/kurisucore/data/Punishment.java
+++ src/main/java/dev/vrremi/kurisucore/data/Punishment.java
@@ -12,5 +12,9 @@ public class Punishment {
     private final String reason, punisher;
     private final long created, timeout;
 
+    public Punishment(PunishmentType punishmentType, String reason, String punisher, long created, long timeout) {
+        this(null, punishmentType, reason, punisher, created, timeout);
+    }
+
     
 }
