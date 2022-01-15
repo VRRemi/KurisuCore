e06eba8a7e97c50244ad163eabb52ad45c0a64a9
VRRemi <business@vrremi.xyz>
2022-01-15 13:06:10 -0800

    Create User.java

---
+++ src/main/java/dev/vrremi/kurisucore/data/User.java
@@ -0,0 +1,15 @@
+package dev.vrremi.kurisucore.data;
+
+import dev.vrremi.kurisucore.KurisuCore;
+import dev.vrremi.kurisucore.data.menu.Menu;
+import dev.vrremi.kurisucore.manager.data.UserDataManager;
+import dev.vrremi.kurisucore.utils.CC;
+import dev.vrremi.kurisucore.utils.ConfigUtils;
+import lombok.Getter;
+import lombok.Setter;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+
+public class User {
+}
