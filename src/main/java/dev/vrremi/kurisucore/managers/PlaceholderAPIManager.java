b56e478b87f7eafe179fce07a2d260784b5bbe74
VRRemi <business@vrremi.xyz>
2022-01-15 11:58:56 -0800

    Create PlaceholderAPIManager.java

---
+++ src/main/java/dev/vrremi/kurisucore/managers/PlaceholderAPIManager.java
@@ -0,0 +1,15 @@
+package dev.vrremi.kurisucore.managers;
+
+import dev.vrremi.kurisucore.KurisuCore;
+import dev.vrremi.kurisucore.data.Rank;
+import dev.vrremi.kurisucore.data.Tag;
+import dev.vrremi.kurisucore.data.User;
+import dev.vrremi.kurisucore.utils.CC;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class PlaceholderAPIManager {
+}
