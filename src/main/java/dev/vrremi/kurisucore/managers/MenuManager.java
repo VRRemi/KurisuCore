406c436251d6008d6bdccbd001dfb62287d1a83d
VRRemi <business@vrremi.xyz>
2022-01-16 12:11:51 +0900

    Update MenuManager.java

--- src/main/java/dev/vrremi/kurisucore/managers/MenuManager.java
+++ src/main/java/dev/vrremi/kurisucore/managers/MenuManager.java
@@ -1,6 +1,14 @@
 package dev.vrremi.kurisucore.managers;
 
-
+import dev.vrremi.kurisucore.KurisuCore;
+import dev.vrremi.kurisucore.data.User;
+import dev.vrremi.kurisucore.data.menu.Menu;
+import dev.vrremi.kurisucore.menus.GrantDurationMenu;
+import dev.vrremi.kurisucore.menus.GrantMainMenu;
+import dev.vrremi.kurisucore.menus.TagsMenu;
 
 public class MenuManager {
+
+    
+
 }
