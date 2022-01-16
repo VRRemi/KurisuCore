e829330c35ba7d3ac47dad76f41d0a737de2acc8
VRRemi <business@vrremi.xyz>
2022-01-15 18:23:29 -0800

    Update menus.java

--- src/main/java/dev/vrremi/kurisucore/data/menu/menus.java
+++ src/main/java/dev/vrremi/kurisucore/data/menu/menus.java
@@ -1,4 +1,9 @@
 package dev.vrremi.kurisucore.data.menu;
 
 public class menus {
+
+    public static final String GRANT_MAIN_MENU = "GrantMainMenu";
+    public static final String GRANT_DURATION_MENU = "GrantDurationMenu";
+    public static final String TAGS_MENU = "TagsMenu";
+    
 }
