bb6975e47f36bb27f95989fe81cc81d30ff6b7c6
VRRemi <business@vrremi.xyz>
2022-01-15 18:25:34 -0800

    Update page.java

--- src/main/java/dev/vrremi/kurisucore/data/menu/page.java
+++ src/main/java/dev/vrremi/kurisucore/data/menu/page.java
@@ -1,6 +1,12 @@
 package dev.vrremi.kurisucore.data.menu;
 
-
+import dev.vrremi.kurisucore.utils.CC;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class page {
 
