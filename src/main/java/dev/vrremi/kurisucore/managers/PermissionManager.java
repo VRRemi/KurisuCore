6c5737dfe290f4a181a99c3ad0c9611a5d3c0068
VRRemi <business@vrremi.xyz>
2022-01-16 11:56:44 +0900

    Update PermissionManager.java

--- src/main/java/dev/vrremi/kurisucore/managers/PermissionManager.java
+++ src/main/java/dev/vrremi/kurisucore/managers/PermissionManager.java
@@ -1,4 +1,11 @@
 package dev.vrremi.kurisucore.managers;
 
+import dev.vrremi.kurisucore.KurisuCore;
+import dev.vrremi.kurisucore.data.CustomPermissible;
+import dev.vrremi.kurisucore.data.Permission;
+import dev.vrremi.kurisucore.data.Rank;
+import dev.vrremi.kurisucore.data.User;
+import dev.vrremi.kurisucore.utils.Threading;
+
 public class PermissionManager {
 }
