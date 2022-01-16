10632f8b74e4b360d93f96e65d4492d4b33b3932
VRRemi <business@vrremi.xyz>
2022-01-15 17:05:13 -0800

    Create menu.java

---
+++ src/main/java/dev/vrremi/kurisucore/data/menu/menu.java
@@ -0,0 +1,13 @@
+package dev.vrremi.kurisucore.data.menu;
+
+import lombok.Getter;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
+import java.util.LinkedList;
+
+public abstract class menu {
+
+    
+
+}
