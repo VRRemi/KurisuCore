7bbf8e876a2bcd585c14ea5c5c46d98d30763bbe
VRRemi <business@vrremi.xyz>
2022-01-16 11:38:32 +0900

    Update TagManager.java

--- src/main/java/dev/vrremi/kurisucore/managers/TagManager.java
+++ src/main/java/dev/vrremi/kurisucore/managers/TagManager.java
@@ -5,5 +5,8 @@ import dev.vrremi.kurisucore.data.Tag;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
 public class TagManager {
 }
