eb3b316408d7336d339996e964a8ed39f52849f5
VRRemi <business@vrremi.xyz>
2022-01-15 18:30:34 -0800

    Update SlotAction.java

--- src/main/java/dev/vrremi/kurisucore/data/menu/SlotAction.java
+++ src/main/java/dev/vrremi/kurisucore/data/menu/SlotAction.java
@@ -10,5 +10,18 @@ public enum SlotAction {
     APPLY_1_MONTH,
     APPLY_3_MONTH;
 
-    
+    public long getTime() {
+        switch (this) {
+            case APPLY_7_DAYS:
+                return 604800000 + System.currentTimeMillis();
+            case APPLY_1_MONTH:
+                return 2592000000L + System.currentTimeMillis();
+            case APPLY_3_MONTH:
+                return 7776000000L + System.currentTimeMillis();
+            case APPLY_PERMANENT:
+                return Long.MAX_VALUE;
+        }
+        return 0;
+    }
+
 }
