bc4d5ecba9583c7839fa4ae5fa82056b7de5c236
VRRemi <business@vrremi.xyz>
2022-01-16 11:45:08 +0900

    Update RankManager.java

--- src/main/java/dev/vrremi/kurisucore/managers/RankManager.java
+++ src/main/java/dev/vrremi/kurisucore/managers/RankManager.java
@@ -1,5 +1,10 @@
 package dev.vrremi.kurisucore.managers;
 
+import dev.vrremi.kurisucore.KurisuCore;
+import dev.vrremi.kurisucore.data.Rank;
+import dev.vrremi.kurisucore.utils.Threading;
+import org.bukkit.Bukkit;
+
 
 
 public class RankManager {
