51cea02e4b9dba03a2f983d53fae4465f0f11db5
VRRemi <business@vrremi.xyz>
2022-01-16 11:51:21 +0900

    Update PunishmentManager.java

--- src/main/java/dev/vrremi/kurisucore/managers/PunishmentManager.java
+++ src/main/java/dev/vrremi/kurisucore/managers/PunishmentManager.java
@@ -1,5 +1,11 @@
 package dev.vrremi.kurisucore.managers;
 
+import dev.vrremi.kurisucore.KurisuCore;
+import dev.vrremi.kurisucore.data.Punishment;
+import dev.vrremi.kurisucore.data.PunishmentType;
+import dev.vrremi.kurisucore.data.User;
+import org.bukkit.entity.Player;
+
 
 
 public class PunishmentManager {
