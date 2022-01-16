905e7514c13a943b9cf91bc4602ca89a8a6fbe8b
VRRemi <business@vrremi.xyz>
2022-01-16 12:38:36 +0900

    Update ConnectionPoolManager.java

--- src/main/java/dev/vrremi/kurisucore/managers/ConnectionPoolManager.java
+++ src/main/java/dev/vrremi/kurisucore/managers/ConnectionPoolManager.java
@@ -1,6 +1,8 @@
 package dev.vrremi.kurisucore.managers;
 
-
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import dev.vrremi.kurisucore.KurisuCore;
 
 public class ConnectionPoolManager {
 }
