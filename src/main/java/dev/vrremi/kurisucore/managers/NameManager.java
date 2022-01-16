89544c8ded6b977e905c3d8837bae464e7d1ade0
VRRemi <business@vrremi.xyz>
2022-01-16 12:06:43 +0900

    Update NameManager.java

--- src/main/java/dev/vrremi/kurisucore/managers/NameManager.java
+++ src/main/java/dev/vrremi/kurisucore/managers/NameManager.java
@@ -1,9 +1,14 @@
 package dev.vrremi.kurisucore.managers;
 
+import dev.vrremi.kurisucore.KurisuCore;
+import dev.vrremi.kurisucore.data.Rank;
+import dev.vrremi.kurisucore.data.Tag;
+import dev.vrremi.kurisucore.data.User;
+import dev.vrremi.kurisucore.utils.CC;
 
 
 public class NameManager {
 
-    
+
 
 }
