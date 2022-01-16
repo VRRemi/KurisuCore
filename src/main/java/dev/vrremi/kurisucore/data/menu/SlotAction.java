401e53d297a16b1cc1241e0f85e1e46c4837b943
VRRemi <business@vrremi.xyz>
2022-01-15 18:30:20 -0800

    Create SlotAction.java

---
+++ src/main/java/dev/vrremi/kurisucore/data/menu/SlotAction.java
@@ -0,0 +1,14 @@
+package dev.vrremi.kurisucore.data.menu;
+
+public enum SlotAction {
+
+    APPLY_TAG,
+    RESET_TAG,
+    APPLY_RANK,
+    APPLY_PERMANENT,
+    APPLY_7_DAYS,
+    APPLY_1_MONTH,
+    APPLY_3_MONTH;
+
+    
+}
