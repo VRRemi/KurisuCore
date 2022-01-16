95940708ca3ca8d242792750df9a50beecc1d967
VRRemi <business@vrremi.xyz>
2022-01-16 11:41:15 +0900

    Update TableManager.java

--- src/main/java/dev/vrremi/kurisucore/managers/TableManager.java
+++ src/main/java/dev/vrremi/kurisucore/managers/TableManager.java
@@ -1,6 +1,11 @@
 package dev.vrremi.kurisucore.managers;
 
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class TableManager {
+
+    
+
 }
