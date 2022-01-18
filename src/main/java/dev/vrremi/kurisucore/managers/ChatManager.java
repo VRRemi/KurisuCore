1ad0d6baabeac565eacac59cac387c44de26a5be
VRRemi <business@vrremi.xyz>
2022-01-18 09:09:56 -0800

    Update ChatManager.java

--- src/main/java/dev/vrremi/kurisucore/managers/ChatManager.java
+++ src/main/java/dev/vrremi/kurisucore/managers/ChatManager.java
@@ -1,4 +1,10 @@
 package dev.vrremi.kurisucore.managers;
 
+import dev.vrremi.kurisucore.utils.ConfigUtils;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang.StringUtils;
+import org.bukkit.Bukkit;
+
 public class ChatManager {
 }
