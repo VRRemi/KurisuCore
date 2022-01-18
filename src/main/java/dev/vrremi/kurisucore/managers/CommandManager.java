b970601c252363b7252032e13f79a7ae8c64f466
VRRemi <business@vrremi.xyz>
2022-01-18 09:04:16 -0800

    Update CommandManager.java

--- src/main/java/dev/vrremi/kurisucore/managers/CommandManager.java
+++ src/main/java/dev/vrremi/kurisucore/managers/CommandManager.java
@@ -1,4 +1,9 @@
 package dev.vrremi.kurisucore.managers;
 
+import dev.vrremi.kurisucore.commands.*;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandMap;
+
 public class CommandManager {
 }
