86f3e916a69e9ee5c04d73aa43e71c03ee4ce46d
VRRemi <business@vrremi.xyz>
2022-01-18 09:14:48 -0800

    Update RankDataManager.java

--- src/main/java/dev/vrremi/kurisucore/managers/data/RankDataManager.java
+++ src/main/java/dev/vrremi/kurisucore/managers/data/RankDataManager.java
@@ -3,5 +3,12 @@ package dev.vrremi.kurisucore.managers.data;
 import dev.vrremi.kurisucore.data.Permission;
 import dev.vrremi.kurisucore.data.Rank;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class RankDataManager {
 }
