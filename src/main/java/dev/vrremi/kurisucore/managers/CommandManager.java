942daaf162c35715f30da6e5ab61f1f754246c90
VRRemi <business@vrremi.xyz>
2022-01-18 09:06:48 -0800

    Update CommandManager.java

--- src/main/java/dev/vrremi/kurisucore/managers/CommandManager.java
+++ src/main/java/dev/vrremi/kurisucore/managers/CommandManager.java
@@ -13,6 +13,23 @@ public class CommandManager {
         registerCommands();
     }
 
-    
+    public void registerCommands() {
+        registerCommand(new TagCommand());
+        registerCommand(new BanCommand());
+        registerCommand(new MuteCommand());
+        registerCommand(new KickCommand());
+        registerCommand(new WarnCommand());
+        registerCommand(new GrantCommand());
+        registerCommand(new RankCommand());
+        registerCommand(new UserCommand());
+        registerCommand(new HistoryCommand());
+        registerCommand(new GameModeCommand());
+        registerCommand(new MessageCommand());
+        registerCommand(new ReplyCommand());
+        registerCommand(new BroadcastCommand());
+        registerCommand(new ChatManagerCommand());
+        registerCommand(new StaffChatCommand());
+
+    }
 
 }
