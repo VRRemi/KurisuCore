68ada62217c360b6b922ecce1c917a4278262caa
VRRemi <business@vrremi.xyz>
2022-01-17 16:08:15 -0800

    Update ConfigManager.java

--- src/main/java/dev/vrremi/kurisucore/managers/ConfigManager.java
+++ src/main/java/dev/vrremi/kurisucore/managers/ConfigManager.java
@@ -1,6 +1,8 @@
 package dev.vrremi.kurisucore.managers;
 
-
+import dev.vrremi.kurisucore.KurisuCore;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 public class ConfigManager {
 }
