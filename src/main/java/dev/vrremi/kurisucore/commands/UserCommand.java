886c4949dfc3e021c26e975d7b41270b3c388e65
VRRemi <business@vrremi.xyz>
2022-01-25 11:09:04 -0800

    Update UserCommand.java

--- src/main/java/dev/vrremi/kurisucore/commands/UserCommand.java
+++ src/main/java/dev/vrremi/kurisucore/commands/UserCommand.java
@@ -3,5 +3,6 @@ package dev.vrremi.kurisucore.commands;
 import dev.vrremi.kurisucore.KurisuCore;
 import dev.vrremi.kurisucore.data.Permission;
 import dev.vrremi.kurisucore.data.Rank;
+import dev.vrremi.kurisucore.data.User;
 public class UserCommand {
 }
