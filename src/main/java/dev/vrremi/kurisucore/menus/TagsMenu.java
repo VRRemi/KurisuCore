3ffdcf009d4161a85d22a877117b2a2125510fa0
VRRemi <business@vrremi.xyz>
2022-01-25 01:23:28 -0800

    Update TagsMenu.java

--- src/main/java/dev/vrremi/kurisucore/menus/TagsMenu.java
+++ src/main/java/dev/vrremi/kurisucore/menus/TagsMenu.java
@@ -34,3 +34,4 @@ public class TagsMenu extends Menu {
                 Inventory inventory = Bukkit.createInventory(null, 54, CC.color("&b&lTAGS"));
                 fill(inventory);
                 User user  = KurisuCore.getUserManager().getUser(player);
+                if (user == null) return inventory;
