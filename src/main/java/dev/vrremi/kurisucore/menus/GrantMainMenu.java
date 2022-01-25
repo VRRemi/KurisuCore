0d5ff76b90cf956735f07a26006f6b4c440fc8a5
VRRemi <business@vrremi.xyz>
2022-01-25 01:05:35 -0800

    Update GrantMainMenu.java

--- src/main/java/dev/vrremi/kurisucore/menus/GrantMainMenu.java
+++ src/main/java/dev/vrremi/kurisucore/menus/GrantMainMenu.java
@@ -12,5 +12,6 @@ import dev.vrremi.kurisucore.utils.LineUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 public class GrantMainMenu {
 }
