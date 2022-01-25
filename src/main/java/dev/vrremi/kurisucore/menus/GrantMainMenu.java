5fbaf642365f9cb5484f03c6d24d6bcedfb731d5
VRRemi <business@vrremi.xyz>
2022-01-25 01:12:56 -0800

    Update GrantMainMenu.java

--- src/main/java/dev/vrremi/kurisucore/menus/GrantMainMenu.java
+++ src/main/java/dev/vrremi/kurisucore/menus/GrantMainMenu.java
@@ -45,3 +45,4 @@ public class GrantMainMenu extends Menu {
                     LineUtils.addHeader(lines, player);
                     String name = user.getTarget() == null ? "null" : user.getTarget().getName();
                     lines.add("&7Click to grant &b&n" + name + "&r &7with " + rank.getColor() + rank.getName());
+                    LineUtils.addHeader(lines, player);
