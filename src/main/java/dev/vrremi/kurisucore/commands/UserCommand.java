ccd29eb83d4d3a70af5986afb552619130421971
VRRemi <business@vrremi.xyz>
2022-01-25 11:50:30 -0800

    Update UserCommand.java

--- src/main/java/dev/vrremi/kurisucore/commands/UserCommand.java
+++ src/main/java/dev/vrremi/kurisucore/commands/UserCommand.java
@@ -50,3 +50,5 @@ public class UserCommand {
                             lines.add("&fRanks&7: &b" + ranks.substring(0, ranks.length() - 4));
                             if (user.getActivePermissionCount() > 0) {
                                 lines.add(CC.color("&fPermissions&7: (&f" + user.getActivePermissionCount() + "&7)"));
+                                StringBuilder permissions = new StringBuilder();
+                                for (Permission permission : user.getPermissions()) {
