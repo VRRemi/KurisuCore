abcc0d92c901ce24bbac1117a3573eac1dd55d42
VRRemi <business@vrremi.xyz>
2022-01-25 01:19:43 -0800

    Update TagsMenu.java

--- src/main/java/dev/vrremi/kurisucore/menus/TagsMenu.java
+++ src/main/java/dev/vrremi/kurisucore/menus/TagsMenu.java
@@ -1,5 +1,23 @@
 package dev.vrremi.kurisucore.menus;
 
+import dev.vrremi.kurisucore.data.Tag;
+import dev.vrremi.kurisucore.data.User;
+import dev.vrremi.kurisucore.data.menu.Menu;
+import dev.vrremi.kurisucore.data.menu.Menus;
+import dev.vrremi.kurisucore.data.menu.Page;
+import dev.vrremi.kurisucore.data.menu.SlotAction;
+import dev.vrremi.kurisucore.utils.CC;
+import dev.vrremi.kurisucore.utils.ConfigUtils;
+import dev.vrremi.kurisucore.utils.LineUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class TagsMenu {
 }
