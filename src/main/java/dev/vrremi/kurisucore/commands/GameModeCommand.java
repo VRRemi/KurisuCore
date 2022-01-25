9a7fddb29e2623ac449cba47b8ae29ba3b164a27
VRRemi <business@vrremi.xyz>
2022-01-25 12:20:28 -0800

    Update GameModeCommand.java

--- src/main/java/dev/vrremi/kurisucore/commands/GameModeCommand.java
+++ src/main/java/dev/vrremi/kurisucore/commands/GameModeCommand.java
@@ -4,5 +4,6 @@ import dev.vrremi.kurisucore.KurisuCore;
 import dev.vrremi.kurisucore.utils.CC;
 import dev.vrremi.kurisucore.utils.ConfigUtils;
 import dev.vrremi.kurisucore.utils.Server;
+import org.bukkit.Bukkit;
 public class GameModeCommand {
 }
