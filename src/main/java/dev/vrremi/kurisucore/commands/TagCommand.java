7f7afd4590e2a5767ba3f45a06acb8b412288b7c
VRRemi <business@vrremi.xyz>
2022-01-25 12:05:11 -0800

    Update TagCommand.java

--- src/main/java/dev/vrremi/kurisucore/commands/TagCommand.java
+++ src/main/java/dev/vrremi/kurisucore/commands/TagCommand.java
@@ -2,5 +2,6 @@ package dev.vrremi.kurisucore.commands;
 
 import dev.vrremi.kurisucore.KurisuCore;
 import dev.vrremi.kurisucore.data.User;
+import dev.vrremi.kurisucore.data.menu.Menus;
 public class TagCommand {
 }
