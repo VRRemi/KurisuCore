4115e91039cca696b195df9ed85c67db6375d2f4
VRRemi <business@vrremi.xyz>
2022-01-25 01:05:20 -0800

    Update GrantMainMenu.java

--- src/main/java/dev/vrremi/kurisucore/menus/GrantMainMenu.java
+++ src/main/java/dev/vrremi/kurisucore/menus/GrantMainMenu.java
@@ -4,5 +4,7 @@ import dev.vrremi.kurisucore.KurisuCore;
 import dev.vrremi.kurisucore.data.Rank;
 import dev.vrremi.kurisucore.data.User;
 import dev.vrremi.kurisucore.data.menu.Menu;
+import dev.vrremi.kurisucore.data.menu.Menus;
+import dev.vrremi.kurisucore.data.menu.Page;
 public class GrantMainMenu {
 }
