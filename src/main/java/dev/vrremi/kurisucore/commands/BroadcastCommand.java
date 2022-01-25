d2b156aa5e0e3e3ec2547de57ba8399335e50fff
VRRemi <business@vrremi.xyz>
2022-01-25 01:31:27 -0800

    Update BroadcastCommand.java

--- src/main/java/dev/vrremi/kurisucore/commands/BroadcastCommand.java
+++ src/main/java/dev/vrremi/kurisucore/commands/BroadcastCommand.java
@@ -3,5 +3,6 @@ package dev.vrremi.kurisucore.commands;
 
 import dev.vrremi.kurisucore.KurisuCore;
 import dev.vrremi.kurisucore.utils.CC;
+import dev.vrremi.kurisucore.utils.ConfigUtils;
 public class BroadcastCommand {
 }
