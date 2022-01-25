4d979700bf95c2971de78c2df06b4e20570e9002
VRRemi <business@vrremi.xyz>
2022-01-24 16:59:02 -0800

    Update GrantDurationMenu.java

--- src/main/java/dev/vrremi/kurisucore/menus/GrantDurationMenu.java
+++ src/main/java/dev/vrremi/kurisucore/menus/GrantDurationMenu.java
@@ -88,6 +88,7 @@ public class GrantDurationMenu extends Menu {
                         ConfigUtils.sendMessage(player, "player-not-online", new HashMap<String, String>() {{
                             put("{player}", user.getTarget() == null ? "null" : user.getTarget().getName());
                         }});
+                    }
     }
 
 }
