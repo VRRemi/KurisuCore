dd85491e8f1d0351e15511ee1ae69dc46642705e
VRRemi <business@vrremi.xyz>
2022-01-28 09:20:26 -0800

    Update BanCommand.java

--- src/main/java/dev/vrremi/kurisucore/commands/BanCommand.java
+++ src/main/java/dev/vrremi/kurisucore/commands/BanCommand.java
@@ -194,3 +194,9 @@ public class BanCommand extends Command {
                                 ConfigUtils.sendMessage(sender, "player-doesnt-exist", new HashMap<String, String>() {{
                                     put("{player}", args[0]);
                                 }});
+                            }
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        } finally {
+                            ConnectionPoolManager.close(connection);
+                        }
